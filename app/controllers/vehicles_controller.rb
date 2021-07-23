d993c3c8575ba795f02a43195688b65a0938bdd4
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-22 21:37:13 -0500

    make vehicles create action and associated route

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,4 +3,16 @@ class VehiclesController < ApplicationController
     vehicles = Vehicle.all
     render json: vehicles.as_json
   end
+
+  def create
+    vehicle = Vehicle.new(
+      user_id: params[:user_id],
+      make: params[:make],
+      model: params[:model],
+      color: params[:color],
+      plate: params[:plate],
+    )
+    vehicle.save
+    render json: vehicle.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
 
   get "/vehicles" => "vehicles#index"
+  post "/vehicles" => "vehicles#create"
 end
