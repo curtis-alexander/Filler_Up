2eacbafb60e26f6c2b55b72e411e4024584318ea
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-22 21:17:25 -0500

    make user create function with associated route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      password: params[:password],
+    )
+    user.save!
+    render json: user.as_json
+  end
 end
