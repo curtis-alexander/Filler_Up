26462795da8ea9ae632d5065bf4701e9a7d3ebf0
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-22 19:03:22 -0500

    turn on bcrypt gem and add users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(first_name: "Amy", last_name: "Glass", email: "aglass@gmail.com", phone_number: "4145557410", password: "password")
+User.create!(first_name: "Bill", last_name: "Torque", email: "torque@bill.com", phone_number: "6085558897", password: "password")
+User.create!(first_name: "Tom", last_name: "Klans", email: "tk@books.com", phone_number: "8085552248", password: "password")
+User.create!(first_name: "Rachel", last_name: "Briggs", email: "rachel@briggs.com", phone_number: "7185555859", password: "password")
+User.create!(first_name: "Steve", last_name: "Saleen", email: "saleen@racing.com", phone_number: "6085559959", password: "password")
+User.create!(first_name: "Richard", last_name: "Rawls", email: "gas@monkey.com", phone_number: "2815552252", password: "password")
+User.create!(first_name: "Brian", last_name: "Rice", email: "brian@actualize.com", phone_number: "5555555555", password: "password")
