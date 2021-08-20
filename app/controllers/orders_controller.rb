28ace917e9f1a52013db5e233ce3e55e597818f6
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-08-19 20:12:21 -0500

    add total to order params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,7 @@ class OrdersController < ApplicationController
       dollar_amount: params[:dollar_amount],
       location: params[:location],
       vehicle_id: params[:vehicle_id],
+      total: params[:total],
     )
     order.save
     render json: order.as_json
